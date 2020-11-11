e46335848e8c7fe629f84bd50e6395a299f5484c
jotkafomat <k-j@xl.wp.pl>
2020-11-11 20:03:07 +0000

    add Note Controller

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,5 +1,19 @@
-function howdy() {
-  var element = document.getElementById('app');
-  element.innerHTML = 'Howdy!!!';
+
+class NoteController{
+  constructor(noteList){
+
+    this.noteList = new NoteList();
+    this.noteList.createNote("Favourite drink: seltzer");
+    this.noteListView = new NoteListView(this.noteList);
+
+  }
+
+  getsHtml(){
+
+    var element = document.getElementById('app');
+    element.innerHTML = this.noteListView.htmlString();
+
+  }
 }
-howdy();
+let myNoteController = new NoteController();
+myNoteController.getsHtml();
