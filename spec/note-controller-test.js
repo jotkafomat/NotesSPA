b4587a4baaa6e0394d32d52dbcd7429fceb37cf7
Emily Sesto <emilyalicesesto@gmail.com>
2020-11-12 16:58:07 +0000

    Mocked tests for note controller hash change

--- spec/note-controller-test.js
+++ spec/note-controller-test.js
@@ -11,7 +11,7 @@
       this.noteList = notelist;
     }
     htmlString() {
-      return "<ul><li><div>Favourite drink: seltzer</div></li></ul>"
+      return `<ul><li><a href="#notes/0">Favourite drink: seltzer</a></li></ul>`
     }
   }
 
@@ -28,10 +28,25 @@
     var doubleNoteList = new DoubleNotelist();
 
     var noteController = new NoteController(doubleNoteList, DoubleNotelistView);
-    assert.isTrue(noteController.getsHtml() === "<ul><li><div>Favourite drink: seltzer</div></li></ul>" )
+    assert.isTrue(noteController.getsHtml() === `<ul><li><a href="#notes/0">Favourite drink: seltzer</a></li></ul>` )
     console.log("testGetsHtm Green");
+  }
+
+  function changesHash() {
+    var fakeClick = function (elem) {
+      var event = new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+        view: window
+      });
+    
+    }
+    fakeClick()
+    var path = document.URL.split("#")[1];
+    assert.isTrue(path === "notes/0");
   }
 
   testGetsHtml();
   testNoteControllerCanBeInstantiated();
+  changesHash();
   })(this);
