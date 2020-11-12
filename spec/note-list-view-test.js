d3e8075f05321744cff8a39db35d514e92cac51c
Emily Sesto <emilyalicesesto@gmail.com>
2020-11-12 16:35:55 +0000

    Tests passing for correct url insertion with note id

--- spec/note-list-view-test.js
+++ spec/note-list-view-test.js
@@ -5,14 +5,14 @@
     noteList.createNote("test");
     var noteListView = new NoteListView(noteList);
 
-    assert.isTrue(noteListView.htmlString() === "<ul><li><div>test</div></li></ul>")
+    assert.isTrue(noteListView.htmlString() === `<ul><li><a href="#notes/0">test</a></li></ul>`)
   };
 
   function returnsFirstTwentyOnly(){
     var noteList = new NoteList();
     noteList.createNote("I am longer than twenty characters");
     var noteListView = new NoteListView(noteList);
-    assert.isTrue(noteListView.htmlString() === "<ul><li><div>I am longer than twe</div></li></ul>")
+    assert.isTrue(noteListView.htmlString() === `<ul><li><a href="#notes/0">I am longer than twe</a></li></ul>`)
   };
 
   testHtmlString();
