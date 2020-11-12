814f1a6fa9dc5e10a03c55bdfe0e6838f102fd86
jotkafomat <k-j@xl.wp.pl>
2020-11-12 15:22:17 +0000

    mocking

--- spec/note-controller-test.js
+++ spec/note-controller-test.js
@@ -3,6 +3,7 @@
   class DoubleNotelist {
     createNote() {
     }
+
   }
 
   class DoubleNotelistView {
@@ -11,28 +12,32 @@
     }
   }
 
+
+
   function testNoteControllerCanBeInstantiated() {
+
     var doubleNoteList = new DoubleNotelist();
+    var doubleNotelistView = new DoubleNotelistView
     var noteController = new NoteController(doubleNoteList);
 
     assert.isTrue(noteController instanceof NoteController);
     console.log("NoteControllerCanBeInstantiated Green");
   }
 
-  // function testGetsHtml() {
-  //   var doubleNoteList = new DoubleNotelist();
-  //
-  //   var noteController = new NoteController(doubleNoteList);
-  //
-  //   var doubleNotelistView = new DoubleNotelistView();
-  //
-  //
-  //
-  //   assert.isTrue(noteController.getsHtml() === "<ul><li><div>Favourite drink: seltzer</div></li></ul>" )
-  //
-  //   console.log("testGetsHtm Green");
-  // }
+  function testGetsHtml() {
+    var doubleNoteList = new DoubleNotelist();
+
+    var noteController = new NoteController(doubleNoteList);
+
+    var doubleNotelistView = new DoubleNotelistView();
 
 
+
+    assert.isTrue(noteController.getsHtml() === "<ul><li><div>Favourite drink: seltzer</div></li></ul>" )
+
+    console.log("testGetsHtm Green");
+  }
+
+  testGetsHtml();
   testNoteControllerCanBeInstantiated();
   })(this);
