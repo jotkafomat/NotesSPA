486fb910de3027d3a8b400769ee8d264265ac5ee
jotkafomat <k-j@xl.wp.pl>
2020-11-13 16:39:42 +0000

    finish step 9

--- src/note-controller.js
+++ src/note-controller.js
@@ -12,6 +12,29 @@ class NoteController{
     var element = document.getElementById('app');
     return element.innerHTML = this.noteListView.htmlString();
   }
+
 }
+
 let myNoteController = new NoteController();
 myNoteController.getsHtml();
+
+makeUrlChangeShowNoteForCurrentPage();
+
+ function makeUrlChangeShowNoteForCurrentPage() {
+   window.addEventListener("hashchange", showNoteForCurrentPage);
+ };
+
+ function showNoteForCurrentPage() {
+   showNote(getNoteFromUrl(window.location));
+ };
+
+ function getNoteFromUrl(location) {
+   return location.hash.split("/")[1];
+ };
+
+ function showNote(id) {
+   var noteToShow = myNoteController.noteList.notes.filter(note => note.id == id)[0];
+   var singleNoteView = new SingleNoteView(noteToShow)
+   var singleNoteViewHTML = singleNoteView.singleHtmlString()
+   document.getElementById('app').innerHTML = singleNoteViewHTML;
+ };
